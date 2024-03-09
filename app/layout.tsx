7e3839e09b874ee9abeecee443277a84993947ee
Tanuj <tanujnayal2909@gmail.com>
2024-03-09 21:42:49 +0530

    added-follow-functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,10 @@ export default function RootLayout({
               enableSystem
               disableTransitionOnChange
             >
+              <Toaster 
+                theme="light"
+                position="bottom-center"
+              />
               {children}
             </ThemeProvider>
           </body>
