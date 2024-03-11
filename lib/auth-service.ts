d923e61098544304568d0df9aa1601dc72be810e
Tanuj <tanujnayal2909@gmail.com>
2024-03-11 22:55:39 +0530

    added-creator-dashboard

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -1,5 +1,6 @@
 import { currentUser } from "@clerk/nextjs"
 import { db } from "./db";
+import { EarOffIcon } from "lucide-react";
 
 
 export const getSelf = async () => {
@@ -21,4 +22,22 @@ export const getSelf = async () => {
 
     return user;
     
+}
+
+export const getSelfByUsername = async (username: string) => {
+    const self = await currentUser();
+
+    if(!self || !self.username) throw new Error("Unauthorized!");
+
+    const user = await db.user.findUnique({
+        where: {
+            username,
+        }
+    })
+
+    if(!user) throw new Error("User not found!");
+
+    if(self.username !== user.username) throw new Error("Unauthorized!");
+
+    return user;
 }
