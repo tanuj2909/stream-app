c38ec148cf5b220d036feb17edf0e67e47f91d54
Tanuj <tanujnayal2909@gmail.com>
2024-03-12 23:53:12 +0530

    added-chat-settings

--- app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
@@ -16,7 +16,7 @@ export const Wrapper = ({
     return <aside
         className={cn(
             "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
-                collapsed && "w-[70px]"
+            collapsed && "w-[70px]"
         )}
     >
         {children}
---
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Switch } from "@/components/ui/switch";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { updateStream } from "@/actions/stream";
+
+type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly" ;
+
+interface ToggleCardProps {
+    label: string;
+    field: FieldTypes;
+    value: boolean;
+}
+
+export const ToggleCard = ({
+    label,
+    field,
+    value = false
+}: ToggleCardProps) => {
+
+    const [isPending, startTransition] = useTransition();
+
+    const onChange = () => {
+        startTransition(() => {
+            updateStream({ [field]: false })
+            .then(() => toast.success("Chat settings updated!"))
+            .catch(() => toast.error("Something went wrong!"))
+
+        })
+    }
+
+    return(
+        <div className="rounded-xl bg-muted p-6">
+            <div className="flex items-center justify-between">
+                <p className="font-semibold shrink-0">
+                    {label}
+                </p>
+                <div className="space-y-2">
+                    <Switch
+                        onCheckedChange={onChange}
+                        disabled={isPending}
+                        checked={value}
+                    >
+                        {value ? "On" : "Off"}
+                    </Switch>
+                </div>
+            </div>
+        </div>
+    )
+}
