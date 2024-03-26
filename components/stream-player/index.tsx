7f6f40fb91fb0ad9266f63e28b3ccebef7e33b9e
Tanuj <tanujnayal2909@gmail.com>
2024-03-26 23:07:53 +0530

    partial-stream-info-card

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Header, HeaderSkeleton } from "./header";
+import { InfoCard } from "./info-card";
 
 
 interface StreamPlayerProps {
@@ -66,6 +67,12 @@ export const StreamPlayer = ({
                         imageUrl={user.imageUrl}
                         isFollowing={isFollowing}
                         name={stream.name}
+                    />
+                    <InfoCard
+                        hostIdentity={user.id}
+                        viewerIdentity={identity}
+                        name={stream.name}
+                        thumbnailUrl={stream.thumbnail}
                     />
                 </div>
                 <div
